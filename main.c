649fce25a70a48ab505f0ab5d319f85a9340f825
YosefKahlon <yosefkahlon53@gmail.com>
2021-11-05 15:24:04 +0200

    test2

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main()
         }
     }
     printf("\n");
-    printf("The Palindromes numbers are:");
+    printf("The Palindromes are:");
     for (int i = a; i <= b; i++)
     {
         int ans = isPalindrome(i);
